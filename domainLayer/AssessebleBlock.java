eda1e7285de205c557565b20d66ec324e99951f1
Arne Libbrecht <arne.libbrecht@icloud.com>
2020-03-05 22:55:24 +0100

    fix boilerplate

---
+++ domainLayer/AssessebleBlock.java
@@ -0,0 +1,20 @@
+package domainLayer;
+
+public abstract class AssessebleBlock extends Block {
+
+	/**
+	 * 
+	 * @param blockId
+	 */
+	public AssessebleBlock(String blockId) {
+		super(blockId);
+		// TODO - implement AssassebleBlock.AssassebleBlock
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean assess() {
+		// TODO - implement AssassebleBlock.assess
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ domainLayer/BlockType.java
@@ -0,0 +1,11 @@
+package domainLayer;
+
+public enum BlockType {
+	MoveForward,
+	TurnLeft,
+	TurnRight,
+	While,
+	If,
+	Not,
+	WallInFront
+}
