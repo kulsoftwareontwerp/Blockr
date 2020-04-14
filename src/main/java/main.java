bd60e6bbc9bf8c396b454d3f771034b278c30330
Arne Libbrecht <arne.libbrecht@icloud.com>
2020-04-14 19:41:04 +0200

    update pom

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,6 +9,7 @@ public class main {
 
 	public static void main(String[] args) {	
 		GameWorld gameWorld = GameWorldType.createInstance("com.kuleuven.swop.group17.RobotGameWorld.applicationLayer.RobotGameWorld");
+//		GameWorld gameWorld = GameWorldType.createInstance("com.kuleuven.swop.group17.CoolGameWorld.applicationLayer.CoolGameWorld");
 		
 		
 		DomainController dc = new DomainController(gameWorld);
