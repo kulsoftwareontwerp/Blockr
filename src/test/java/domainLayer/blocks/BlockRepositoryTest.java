306fc840b68e38e2b27c549c2694e6a723e0ca57
Arne Libbrecht <arne.libbrecht@icloud.com>
2020-04-24 12:44:31 +0200

    last merge fixes

--- src/test/java/domainLayer/blocks/BlockRepositoryTest.java
+++ src/test/java/domainLayer/blocks/BlockRepositoryTest.java
@@ -3,21 +3,10 @@
  */
 package domainLayer.blocks;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-/**
- * BlockRepositoryTest
- *
- * @version 0.1
- * @author group17
- */
-
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -34,14 +23,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 
-import com.kuleuven.swop.group17.GameWorldApi.Action;
-
-import applicationLayer.GameController;
 import types.BlockCategory;
 import types.BlockType;
 import types.ConnectionType;
