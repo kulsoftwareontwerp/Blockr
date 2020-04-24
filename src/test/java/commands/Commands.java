eab338d627e3bedc43025b803e4f72559465c10f
Arne Libbrecht <arne.libbrecht@icloud.com>
2020-04-24 14:06:23 +0200

    suites

---
+++ src/test/java/commands/Commands.java
@@ -0,0 +1,11 @@
+package commands;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CommandHandlerTest.class })
+public class Commands {
+
+}
