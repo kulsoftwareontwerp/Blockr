55d8c45ebf7bd14b817b4aa2fa26f013908cc099
JonathanDelm <j.delmeiren@gmail.com>
2020-05-16 11:47:36 +0200

    Added tests for guiLayer.ResetCommand

---
+++ src/test/java/guiLayer/commands/ResetCommandTest.java
@@ -0,0 +1,104 @@
+package guiLayer.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+import java.lang.reflect.Field;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import applicationLayer.DomainController;
+import guiLayer.CanvasWindow;
+import guiLayer.types.GuiSnapshot;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ResetCommandTest {
+	
+	@Mock(name="controller")
+	private DomainController controller;
+	
+	@Spy @InjectMocks
+	private ResetCommand command;
+
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	/**
+	 * Test method for {@link guiLayer.commands.DomainMoveCommand#DomainMoveCommand(DomainController, CanvasWindow, GuiSnapshot, GuiSnapshot)}.
+	 */
+	@Test
+	public void testResetCommand_Positive() {
+		try {
+			Field controllerField = ResetCommand.class.getDeclaredField("controller");
+			controllerField.setAccessible(true);
+			assertTrue("controller was not initialised", controllerField.get(command) != null);
+
+			Field executedField = ResetCommand.class.getDeclaredField("executed");
+			executedField.setAccessible(true);
+			assertFalse("executed boolean was not initialised", (boolean) executedField.get(command));
+
+		} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+			fail("One or more of the required fields were not declared.");
+		}
+	}
+	
+	/**
+	 * Test method for {@link guiLayer.commands.ExecuteBlockCommand#execute()}.
+	 */
+	@Test
+	public void testExecute_ExecutedFalse_Positive() {
+		command.execute();
+		
+		verify(controller,atLeastOnce()).resetGameExecution();
+		
+		try {
+			Field executedField = ResetCommand.class.getDeclaredField("executed");
+			executedField.setAccessible(true);
+			assertTrue("executed boolean was not initialised", (boolean) executedField.get(command));
+
+		} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+			fail("One or more of the required fields were not declared.");
+		}
+	}
+	
+	/**
+	 * Test method for {@link guiLayer.commands.ExecuteBlockCommand#execute()}.
+	 */
+	@Test
+	public void testExecute_ExecutedTrue_Positive() {
+		// Quick workaround to set executed boolean true
+		command.execute();
+		
+		command.execute();
+
+		verify(controller,atLeastOnce()).redo();
+	}
+
+}
