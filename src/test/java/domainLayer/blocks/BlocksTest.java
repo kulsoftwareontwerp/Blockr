692bc7dd6d2b4ebc87e0bbbd38a4b8a7b6a6d76a
JonathanDelm <j.delmeiren@gmail.com>
2020-05-20 15:55:34 +0200

    Finished adding tests for changes in Block

--- src/test/java/domainLayer/blocks/BlocksTest.java
+++ src/test/java/domainLayer/blocks/BlocksTest.java
@@ -3,8 +3,9 @@ package domainLayer.blocks;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
@@ -12,25 +13,32 @@ import java.util.Set;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 
 import com.kuleuven.swop.group17.GameWorldApi.GameWorld;
 
+import exceptions.InvalidBlockConnectionException;
 import types.BlockCategory;
 import types.BlockType;
 import types.ConnectionType;
 
 public class BlocksTest {
+	
+	@Rule
+	public ExpectedException exceptionRule = ExpectedException.none();
 
 	private IfBlock block;
 	private IfBlock block2;
 	private IfBlock blockWithSameIdAsBlock;
 	private IfBlock blockWithIdNull;
 	private IfBlock blockWithIdNull2;
-//	private IfBlock spyBlock;
+	private IfBlock spyIfBlock;
+	private ActionBlock spyActionBlock;
 	private ConditionBlock conditionBlock;
 	private UnaryOperatorBlock unaryOperatorBlock;
 	private WhileBlock whileBlock;
@@ -48,7 +56,8 @@ public class BlocksTest {
 		blockWithSameIdAsBlock = new IfBlock("0");
 		blockWithIdNull = new IfBlock(null);
 		blockWithIdNull2 = new IfBlock(null);		
-//		spyBlock = spy(new IfBlock("spy"));
+		spyIfBlock = spy(new IfBlock("spy"));
+		spyActionBlock = spy(new ActionBlock("actionBlock", new BlockType("ACTION", BlockCategory.ACTION)));
 		conditionBlock = spy(new ConditionBlock("conditionBlockId", new BlockType("condition", BlockCategory.CONDITION)));
 		unaryOperatorBlock = new NotBlock("unaryOperatorBlockId");
 		whileBlock = new WhileBlock("whileBlockId");
@@ -137,23 +146,179 @@ public class BlocksTest {
 	}
 
 	/**
-	 * Test method for {@link domainLayer.blocks.Block#equals(Object)}.
+	 * Test method for {@link domainLayer.blocks.Block#clone()}.
 	 */
 	@Test
 	public void testClone_Positive() {
-		assertEquals(block, block.clone());
+		Block clone = (Block) block.clone();
+		assertTrue(clone != block);
+		assertTrue(clone.equals(block));
 	}
 	
-	// TODO: how to test this?
-//	/**
-//	 * Test method for {@link domainLayer.blocks.Block#equals(Object)}.
-//	 */
-//	@Test
-//	public void testClone_WithAllSetters_Positive() {
-//		when(spyBlock.getConditionBlock()).thenReturn(Mockito.mock(ConditionBlock.class));
-//		assertEquals(spyBlock, spyBlock.clone());
-//	}
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#clone()}.
+	 */
+	@Test
+	public void testClone_WithAllIfs_Positive() {
+		block.setConditionBlock(conditionBlock);
+		block.setFirstBlockOfBody(spyIfBlock);
+		block.setNextBlock(spyActionBlock);
+		
+		Block clone = (Block) block.clone();
+		assertTrue(clone != block);
+		assertTrue(clone.equals(block));
+		
+		verify(conditionBlock,atLeastOnce()).clone();
+		verify(spyIfBlock,atLeastOnce()).clone();
+		verify(spyActionBlock,atLeastOnce()).clone();
+	}
+	
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#clone()}.
+	 */
+	@Test
+	public void testClone_Operand_Positive() {
+		notBlock.setOperand(conditionBlock);
+		
+		Block clone = (Block) notBlock.clone();
+		assertTrue(clone != notBlock);
+		
+		verify(conditionBlock,atLeastOnce()).clone();
+	}
+	
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#clone()}.
+	 */
+	@Test
+	public void testSetNextBlock_InvalidBlockConnectionException() {
+		String excMessage = "The new block and/or the connected block is no ExecutableBlock.";
+		exceptionRule.expect(InvalidBlockConnectionException.class);
+		exceptionRule.expectMessage(excMessage);
+		
+		try {
+			block.setNextBlock(notBlock);
+		} catch (InvalidBlockConnectionException e) {
+			assertEquals(excMessage, e.getMessage());
+		}
+		
+		block.setNextBlock(notBlock);
+	}
+	
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#clone()}.
+	 */
+	@Test
+	public void testSetFirstBlockOfBody_InvalidBlockConnectionException() {
+		String excMessage = "The new block and/or the connected block is no ExecutableBlock.";
+		exceptionRule.expect(InvalidBlockConnectionException.class);
+		exceptionRule.expectMessage(excMessage);
+		
+		try {
+			block.setFirstBlockOfBody(notBlock);
+		} catch (InvalidBlockConnectionException e) {
+			assertEquals(excMessage, e.getMessage());
+		}
+		
+		block.setFirstBlockOfBody(notBlock);
+	}
+	
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#setOperand(Block)}.
+	 */
+	@Test
+	public void testParseToValidOperation_InvalidBlockConnectionException() {
+		String excMessage = "This block is no AssessableBlock.";
+		exceptionRule.expect(InvalidBlockConnectionException.class);
+		exceptionRule.expectMessage(excMessage);
+		
+		try {
+			block.setOperand(block2);
+		} catch (InvalidBlockConnectionException e) {
+			assertEquals(excMessage, e.getMessage());
+		}
+		
+		block.setOperand(block2);
+	}
 
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#setOperand(AssessableBlock)}.
+	 */
+	@Test
+	public void testSetOperand_InvalidBlockConnectionException() {
+		String excMessage = "The connected block doesn't have the requested connection.";
+		exceptionRule.expect(InvalidBlockConnectionException.class);
+		exceptionRule.expectMessage(excMessage);
+		
+		try {
+			spyActionBlock.setOperand(notBlock);
+		} catch (InvalidBlockConnectionException e) {
+			assertEquals(excMessage, e.getMessage());
+		}
+		
+		spyActionBlock.setOperand(notBlock);
+	}
+	
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#setNextBlock(ExecutableBlock)}.
+	 */
+	@Test
+	public void testSetNextBlock_Executable_InvalidBlockConnectionException() {
+		String excMessage = "The connected block doesn't have the requested connection.";
+		exceptionRule.expect(InvalidBlockConnectionException.class);
+		exceptionRule.expectMessage(excMessage);
+		
+		try {
+			notBlock.setNextBlock(spyActionBlock);
+		} catch (InvalidBlockConnectionException e) {
+			assertEquals(excMessage, e.getMessage());
+		}
+		
+		notBlock.setNextBlock(spyActionBlock);
+	}
+	
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#setFirstBlockOfBody(ExecutableBlock)}.
+	 */
+	@Test
+	public void testSetFirstBlockOfBody_Executable_InvalidBlockConnectionException() {
+		String excMessage = "The connected block doesn't have the requested connection.";
+		exceptionRule.expect(InvalidBlockConnectionException.class);
+		exceptionRule.expectMessage(excMessage);
+		
+		try {
+			notBlock.setFirstBlockOfBody(spyActionBlock);
+		} catch (InvalidBlockConnectionException e) {
+			assertEquals(excMessage, e.getMessage());
+		}
+		
+		notBlock.setFirstBlockOfBody(spyActionBlock);
+	}
+	
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#setConditionBlock(AssessableBlock)}.
+	 */
+	@Test
+	public void testSetConditionBlock_Assessable_InvalidBlockConnectionException() {
+		String excMessage = "The connected block doesn't have the requested connection.";
+		exceptionRule.expect(InvalidBlockConnectionException.class);
+		exceptionRule.expectMessage(excMessage);
+		
+		try {
+			notBlock.setConditionBlock(conditionBlock);
+		} catch (InvalidBlockConnectionException e) {
+			assertEquals(excMessage, e.getMessage());
+		}
+		
+		notBlock.setConditionBlock(conditionBlock);
+	}
+	
+	/**
+	 * Test method for {@link domainLayer.blocks.Block#getBlockId()}.
+	 */
+	@Test
+	public void testGetBlockId_Positive() {
+		assertEquals("0", block.getBlockId());
+	}
 
 	// ----- TESTS FOR EXECUTABLEBLOCK -----
 	// Already tested indirectly
